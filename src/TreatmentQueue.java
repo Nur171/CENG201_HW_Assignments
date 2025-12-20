0ac2c601ddcb25f3737cb8089ccdcc5691a140a4
nurAksoy <nuraksoy2023@gmail.com>
2025-12-20 22:32:50 +0300

    .

--- src/TreatmentQueue.java
+++ src/TreatmentQueue.java
@@ -1,34 +1,66 @@
 public class TreatmentQueue {
-    private QNode front;
-    private QNode tail;
+    private QNode normalFront;
+    private QNode normalTail;
+    private QNode priorityFront;
+    private QNode priorityTail;
     private int size;
+
+    public TreatmentQueue(){
+        normalFront = null;
+        normalTail = null;
+        priorityFront = null;
+        priorityTail = null;
+        size = 0;
+    }
 
     // Adding element to tail
     public void enqueue(TreatmentRequest request){
         QNode newNode = new QNode(request);
 
-        if (tail == null){
-            front = newNode;
-            tail = newNode;
-        }else {
-            tail.next = newNode;
-            tail = newNode;
+        if (request.priority) {
+            // insert into priority queue
+            if (priorityTail == null) {
+                priorityFront = newNode;
+                priorityTail = newNode;
+            } else {
+                priorityTail.next = newNode;
+                priorityTail = newNode;
+            }
+        }else{
+            // inser into normal queue
+            if (normalTail == null) {
+                normalFront = newNode;
+                normalTail = newNode;
+            } else {
+                normalTail.next = newNode;
+                normalTail = newNode;
         }
         size++;
     }
 
     // Removing the element from the head
     public TreatmentRequest dequeue(){
-        if (front ==null){
-            return null;
+        if (priorityFront != null){
+            TreatmentRequest r = priorityFront.request;
+            priorityFront = priorityFront.next;
+
+            if (priorityFront == null)
+                priorityTail = null;
+
+            size--;
+            return r;
         }
-        if (front == null){
-            tail = null;
+        if (normalFront != null){
+            TreatmentRequest r = normalFront.request;
+            normalFront = normalFront.next;
+
+            if (normalFront == null)
+                normalTail = null;
+
+            size--;
+            return r;
         }
-        TreatmentRequest removed = front.request;
-        front = front.next;
-        size--;
-        return removed;
+        return null;
     }
 
     // The size of the queue
@@ -38,10 +70,12 @@ public class TreatmentQueue {
 
     // Printing the queue
     public void printQueue(){
-        if (front == null){
-            System.out.println("Treatment queue is empty.");
-            return;
-        }
+         System.out.println("Priority Requests:");
+         QNode p = priorityFront;
+         while (p != null){
+
+         }
+
         System.out.println("Current Treatment Requests: ");
         QNode current = front;
         while (current != null) {
--- src/TreatmentRequest.java
+++ src/TreatmentRequest.java
@@ -1,14 +1,16 @@
 public class TreatmentRequest {
     int patientId;
     long arrivalTime;
+    boolean priority; // if true -> high priority patient
 
     public TreatmentRequest(int patientId) {
         this.patientId = patientId;
         this.arrivalTime = System.currentTimeMillis();
+        this.priority = priority;
     }
 
     public String printInfo() {
-        return "Treatment Request { Patient Id: " + patientId + "\n" + "Arrival time: " + arrivalTime + "}";
+        return "Treatment Request { Patient Id: " + patientId + "\n" + "Priority: " + priority + "\n" + "Arrival time: " + arrivalTime + "}";
     }
 
 }
