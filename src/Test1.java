fa8c0f98837dd2cb10ad77b26480ba5d955d2b65
nurAksoy <nuraksoy2023@gmail.com>
2025-12-21 22:41:12 +0300

    .

---
+++ src/Test1.java
@@ -0,0 +1,59 @@
+public class Test1 {
+    public static void main(String[] args) {
+        // Main task 1
+
+        PatientList list = new PatientList();
+
+        // Adding 5 patients
+        Patient a = new Patient(1, "Nur Aksoy", 3, 21);
+        Patient b = new Patient(2, "Ahmet Mutlu", 5, 35);
+        Patient c = new Patient(3, "Ayşe Yılmaz", 7, 47);
+        Patient d = new Patient(4, "Yusuf Atılgan", 1, 13);
+        Patient e = new Patient(5, "Zeynep Korkmaz", 9, 61);
+
+        list.addPatient(a);
+        list.addPatient(b);
+        list.addPatient(c);
+        list.addPatient(d);
+        list.addPatient(e);
+
+        System.out.println("After adding 5 patients:");
+        list.printList();
+        System.out.println("Size: " + list.size());
+        System.out.println();
+
+        System.out.println("*************************************\n");
+
+        // Removing patient
+        boolean removed = list.removePatient(3);
+        if (removed) {
+            System.out.println("Patient with id = 3 is removed");
+        } else {
+            System.out.println("Patient with id = 3 is not found");
+        }
+        list.printList();
+        System.out.println();
+
+        System.out.println("*************************************\n");
+
+        // Finding patient
+        Patient found = list.findPatient(5);
+        if (found != null) {
+            System.out.println("Patient with id = 5 is found.");
+            System.out.println(found.printInfo());
+        } else {
+            System.out.println("Patient with id = 5 is not found");
+        }
+        System.out.println();
+        System.out.println("*************************************\n");
+
+        System.out.println("Final list: ");
+        list.printList();
+        System.out.println("Final size: " + list.size());
+
+        System.out.println("*************************************\n");
+        System.out.println("*************************************\n");
+
+
+    }
+}
