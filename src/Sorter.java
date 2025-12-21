23c37a26142020cc5a6efe16f551f94828b19c01
nurAksoy <nuraksoy2023@gmail.com>
2025-12-21 22:40:58 +0300

    .

---
+++ src/Sorter.java
@@ -0,0 +1,18 @@
+public class Sorter {
+    public static Patient[] sortBySeverity (PatientList list){
+        Patient[] array = list.toArray();
+
+        // Bubble sort
+        for (int i = 0; i < array.length -1 ; i++){
+            for (int j = 0 ; j < array.length -i -1 ; j++){
+                if (array[j].severity < array[ j+1 ].severity){
+                    // Swaping
+                    Patient a = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = a;
+                }
+            }
+        }
+        return array;
+    }
+}
