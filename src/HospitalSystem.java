d3c9144dca3cf3277925e7216145b7539676d138
nurAksoy <nuraksoy2023@gmail.com>
2025-12-21 22:40:43 +0300

    .

---
+++ src/HospitalSystem.java
@@ -0,0 +1,79 @@
+import java.util.HashMap;
+public class HospitalSystem {
+
+        private PatientList patientList;
+        private TreatmentQueue treatmentQueue;
+        private DischargeStack dischargeStack;
+        private HashMap<Integer, Patient> patientMap;
+
+        public HospitalSystem() {
+            patientList = new PatientList();
+            treatmentQueue = new TreatmentQueue();
+            dischargeStack = new DischargeStack();
+            patientMap = new HashMap<>();
+        }
+
+        // Adding new patient
+        public void addPatient(Patient p) {
+            patientList.addPatient(p);
+            patientMap.put(p.id, p);
+        }
+
+        // Adding treatment request
+        public void addTreatmentRequest(int patientId, boolean priority) {
+            if (!patientMap.containsKey(patientId)) {
+                System.out.println("ERROR: Patient not found!");
+                return;
+            }
+            TreatmentRequest request = new TreatmentRequest(patientId, priority);
+            treatmentQueue.enqueue(request);
+        }
+
+        // Adding discharge record
+        public void addDischargeRecord(int patientId) {
+            dischargeStack.push(new DischargeRecord(patientId));
+        }
+
+        // Processing treatment request → move to discharge stack
+        public void processNextTreatment() {
+            TreatmentRequest request = treatmentQueue.dequeue();
+
+            if (request == null) {
+                System.out.println("No treatment requests to process");
+                return;
+            }
+
+            dischargeStack.push(new DischargeRecord(request.patientId));
+            patientList.removePatient(request.patientId);
+            patientMap.remove(request.patientId);
+
+            System.out.println("Processed patient: " + request.patientId);
+        }
+
+        // Printing system status
+        public void printSystem() {
+            System.out.println("*** Patient List ***");
+            patientList.printList();
+
+            System.out.println("\n*** Treatment Queue ***");
+            treatmentQueue.printQueue();
+
+            System.out.println("\n*** Discharge Stack ***");
+            dischargeStack.printStack();
+        }
+
+        // Showing sorted list by severity
+        public void printSortedBySeverity() {
+            Patient[] array = Sorter.sortBySeverity(patientList);
+
+            if (array == null || array.length == 0) {
+                System.out.println("No patients to sort");
+                return;
+            }
+            System.out.println("\n=== Patients Sorted by Severity ===");
+            for (Patient p : array) {
+                System.out.println("* " + p);
+            }
+        }
+    }
+
