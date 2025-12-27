c00d4356601d67afa28c9b63b1f411233747b3ed
nurAksoy <nuraksoy2023@gmail.com>
2025-12-27 17:41:34 +0300

    .

--- src/Test4.java
+++ src/Test4.java
@@ -1,15 +1,15 @@
 public class Test4 {
     public static void main(String[] args) {
 
-        // Task 4
+        // ** Task 4
         HospitalSystem hs = new HospitalSystem();
 
-        // Adding 10 patients
+        // * Adding 10 patients
         for (int i = 0; i <= 9; i++) {
             hs.addPatient(new Patient(1 + i, "Patient " + (i + 1), (i % 10) + 1, 30 + i));
         }
 
-        // Adding 5 normal + 3 priority treatment requests
+        // * Adding 5 normal + 3 priority treatment requests
         hs.addTreatmentRequest(1, false);
         hs.addTreatmentRequest(2, false);
         hs.addTreatmentRequest(3, false);
@@ -20,20 +20,21 @@ public class Test4 {
         hs.addTreatmentRequest(7, true);
         hs.addTreatmentRequest(8, true);
 
-        // Adding 2 discharge records
+        // * Adding 2 discharge records
         hs.addDischargeRecord(15);
         hs.addDischargeRecord(16);
 
-        // Processing 4 treatment requests (priority first)
+        // * Processing 4 treatment requests (priority first)
+        System.out.println("* The processed patients: ");
         hs.processNextTreatment();
         hs.processNextTreatment();
         hs.processNextTreatment();
         hs.processNextTreatment();
 
-        // Printing final system state
+        // * Printing final system state
         hs.printSystem();
 
-        // Sorted list by severity
+        // * Sorted list by severity
         hs.printSortedBySeverity();
 
     }
