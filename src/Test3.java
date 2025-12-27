4f0b124de305962ba0282f27016079d0e79c49db
nurAksoy <nuraksoy2023@gmail.com>
2025-12-27 17:41:27 +0300

    .

--- src/Test3.java
+++ src/Test3.java
@@ -1,10 +1,10 @@
 public class Test3 {
     public static void main(String[] args) {
-        // Main task 3
+        // ** Main task 3
 
         DischargeStack stack = new DischargeStack();
 
-        // Adding 5 discharge records
+        // * Adding 5 discharge records
         stack.push(new DischargeRecord(11));
         stack.push(new DischargeRecord(12));
         stack.push(new DischargeRecord(13));
@@ -18,17 +18,16 @@ public class Test3 {
 
         System.out.println("*************************************\n");
 
-        // Popping 2 element
+        // * Popping 2 element
         System.out.println("Pop 1: " + stack.pop());
         System.out.println("Pop 2: " + stack.pop());
         System.out.println();
         System.out.println("*************************************\n");
 
-        // Remaining stack
+        // * Remaining stack
         System.out.println("Remaining stack:");
         stack.printStack();
         System.out.println("Final size: " + stack.size());
     }
-
 }
 
